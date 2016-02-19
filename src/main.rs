630fe580564c32a40cb47ecf3d65c9a76ae66e73
Chris MacNaughton <chmacnaughton@gmail.com>
2016-02-19 17:05:57 -0500

    parse cli args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+#[macro_use] extern crate clap;
+
+use clap::{Arg, App};
+
+
 fn main() {
-    println!("Hello, world!");
+    let matches = clap::App::new("unlock_ceph")
+        .version(crate_version!())
+        .arg(Arg::with_name("source")
+                            .short("s")
+                            .help("Source directory to move files from")
+                            .takes_value(true))
+        .arg(Arg::with_name("destination")
+                            .short("d")
+                            .help("TMPFS to put files on")
+                            .takes_value(true))
+        .arg(Arg::with_name("vault")
+                            .short("V")
+                            .help("Vault server IP")
+                            .takes_value(true))
+        .arg(Arg::with_name("token")
+                            .short("t")
+                            .help("Vault authentication token")
+                            .takes_value(true))
+        .get_matches();
+
+    println!("Matches is {:?}", matches);
 }
